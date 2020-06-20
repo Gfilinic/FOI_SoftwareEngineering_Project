75cc99111372333bdde4a633cccbd68aadc8b933
ClementConn <62304568+ClementConn@users.noreply.github.com>
2020-06-20 10:54:27 +0200

    Add Measurement

---
+++ Software/DataBase/Measurement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class Measurement
+    {
+        public int Id_measurement { get; set; }
+        public string measurement1 { get; set; }
+
+        public Measurement(int id_measurment, string measurement)
+        {
+            this.Id_measurement = id_measurment;
+            this.measurement1 = measurement;
+        }
+    }
+}
